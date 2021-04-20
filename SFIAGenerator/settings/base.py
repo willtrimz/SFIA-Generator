c3c5adba88242d41b7605cbe13d020d7df80e837
William Trimby <trimbyw@cardiff.ac.uk>
2021-04-20 12:48:29 +0100

    fixed admin styling using js to remove jquery from dom. changed bootstrap js to working version. corrected the address of static folder in base settings

--- SFIAGenerator/settings/base.py
+++ SFIAGenerator/settings/base.py
@@ -19,7 +19,7 @@ DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
